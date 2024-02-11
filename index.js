51b1b58dc7f1c2ec985917a2678a55da7cb6b491
silas <silasgebhart12@gmail.com>
2024-02-11 16:08:35 -0600

    final commit

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+  if (activity === "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  } else if (activity === "bathe my dog") {
+    return `This Saturday, I want to ${activity}!`;
+  } else {
+    return `This Saturday, I want to ${activity}.`;
+  }
+}
+
+function mondayWork(activity = "go to the office") {
+  return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = "*") {
+  return function (adjective = "special") {
+    return `You are ${flair}${adjective}${flair}!`;
+  };
+}
+wrapAdjective("%")("a dedicated programmer");
